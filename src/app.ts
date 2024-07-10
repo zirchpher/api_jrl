e87045df4fc15f4b8e4f83ddf8acf22b68318d71
DuxTec2024 <jorgeromerohinojosa@gmil.com>
2024-07-10 12:51:30 -0500

    finalize con el archivo productos

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
-import productoRoutes from './routes/productoRoutes';
+//import productoRoutes from './routes/productoRoutes';
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api', productoRoutes);
+//app.use('/api', productoRoutes);
 
 export default app;
